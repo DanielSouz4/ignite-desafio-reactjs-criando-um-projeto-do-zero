9a55b221f3b3bdb1fd30bbf27a75a51edff1abaa
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2021-11-26 18:53:02 -0300

    Página home quase finalizada!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import { GetStaticProps } from 'next';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../services/prismic';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +31,79 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <header className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <img src="/images/logo.svg" alt="logo" />
+        </div>
+      </header>
 
-//   // TODO
-// };
+      <main className={styles.mainContainer}>
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <time>
+                    <FiCalendar />
+                    <span>
+                      {format(
+                        new Date(post.first_publication_date),
+                        'd LLL y',
+                        {
+                          locale: ptBR,
+                        }
+                      )}
+                    </span>
+                  </time>
+                  <p>
+                    <FiUser />
+                    <span>{post.data.author}</span>
+                  </p>
+                </div>
+              </a>
+            </Link>
+          ))}
+
+          <p>Carregar mais posts</p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100,
+    }
+  );
+
+  const postsPagination = postsResponse;
+
+  return {
+    props: {
+      postsPagination,
+    },
+    revalidate: 60 * 30, // 30 minutes
+  };
+};
